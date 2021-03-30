b51b25f36ef83f1cc7548583f254e4463be8b314
iotyun <ytpay@qq.com>
2021-03-31 04:59:16 +0800

    修正文件名

--- src/Service.php
+++ src/Service.php
@@ -17,7 +17,7 @@ class Service extends BaseService
     public function register()
     {
         $this->commands([
-            'IotyunTcp' => 'iotyun\\tcp\\command\\tcp',
+            'IotyunTcp' => 'iotyun\\tcp\\command\\Tcp',
         ]);
     }
 }
